3fe44abc9e39df0924e152174bb443012f061180
bkrajor <bkrajor@gmail.com>
2022-04-13 01:17:01 +0530

    Assignment Done

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const bookModel= require("../models/bookModel.js")
 const bookController= require("../controllers/bookController")
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
 
 router.post("/createBook", bookController.createBook  )
 
