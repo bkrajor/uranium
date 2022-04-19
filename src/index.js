3f0a5bd60b0953c8e5bf1137369e29206d4fc173
bkrajor <bkrajor@gmail.com>
2022-04-20 00:38:35 +0530

    Middleware Done!

--- src/index.js
+++ src/index.js
@@ -11,15 +11,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
 
-app.use (
-    function (req, res, next) {
-        console.log ("inside GLOBAL MW");
-        next();
-  }
-  );
+const globalMiddleware = (req, res, next) => {
+    let now = new Date().toLocaleString();
+    console.log(now + " , " + req.ip + " , " + req.originalUrl)
+    next();
+}
+
+app.use(globalMiddleware)
+
 
 app.use('/', route);
 
