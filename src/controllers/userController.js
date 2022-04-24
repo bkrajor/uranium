29ac8194d14603f38855a1c3c926eabb9175d6ce
bkrajor <bkrajor@gmail.com>
2022-04-25 00:05:42 +0530

    StatusCodesAdded

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,62 +3,88 @@ const userModel = require("../models/userModel")
 
 // create user
 const createUser = async function (req, res) {
-  let savedData = await userModel.create(req.body)
-  res.send({ status: true, msg: savedData })
+  try {
+    let savedData = await userModel.create(req.body)
+    res.status(201).send({ msg: savedData })
+  } catch (err) {
+    res.status(500).send({ Error: err.message })
+  }
 }
 
 
 // login user and generate jwt
 const loginUser = async function (req, res) {
-  let user = await userModel.findOne({ emailId: req.body.emailId, password: req.body.password })
-  if (!user) return res.send({ status: false, msg: "username or the password is not correct", })
+  try {
+    let user = await userModel.findOne({ emailId: req.body.emailId, password: req.body.password })
+    if (!user) return res.status(400).send({ msg: "username or the password is not correct", })
 
-  let token = jwt.sign({ userId: user._id.toString() }, "functionup-uranium")
-  res.send({ status: true, JWT: token })
+    let token = jwt.sign({ userId: user._id.toString() }, "functionup-uranium")
+    res.status(200).send({ JWT: token })
+  } catch (err) {
+    res.status(500).send({ Error: err.message })
+  }
+
 }
 
 // get userdata
 const getUserData = async function (req, res) {
-  let userDetails = await userModel.findById(req.params.userId)
-  if (!userDetails)
-    return res.send({ status: false, msg: "No such user exists" })
+  try {
+    let userDetails = await userModel.findById(req.params.userId)
+    if (!userDetails)
+      return res.status(404).send({ msg: "No such user exists" })
 
-  res.send({ status: true, data: userDetails })
+    res.status(200).send({ data: userDetails })
+  } catch (err) {
+    res.status(500).send({ Error: err.message })
+  }
 }
 
 
 // update user
 const updatedUser = async function (req, res) {
-  let userDetails = await userModel.findById(req.params.userId)
-  if (!userDetails)
-    return res.send("No such user exists")
+  try {
+    let userDetails = await userModel.findById(req.params.userId)
+    if (!userDetails)
+      return res.status(404).send("No such user exists")
 
-  let userData = req.body
-  let updatedUser = await userModel.updateOne({ _id: req.params.userId }, { $set: userData }, { new: true })
-  res.send({ status: "updatedUser true", data: updatedUser })
+    let userData = req.body
+    let updatedUser = await userModel.updateOne({ _id: req.params.userId }, { $set: userData }, { new: true })
+    res.status(200).send({ data: updatedUser })
+  } catch (err) {
+    res.status(500).send({ Error: err.message })
+  }
 }
 
 
 // delete user
 const deletedUser = async function (req, res) {
-  let userDetails = await userModel.findById(req.params.userId)
-  if (!userDetails)
-    return res.send({ status: false, msg: "No such user exists" })
+  try {
+    let userDetails = await userModel.findById(req.params.userId)
+    if (!userDetails)
+      return res.status(404).send({ msg: "No such user exists" })
 
-  let deletedUser = await userModel.updateOne({ _id: req.params.userId }, { $set: { isDeleted: true } }, { new: true })
-  res.send({ status: true, msg: deletedUser })
+    let deletedUser = await userModel.updateOne({ _id: req.params.userId }, { $set: { isDeleted: true } }, { new: true })
+    res.status(200).send({ msg: deletedUser })
+  } catch (err) {
+    res.status(500).send({ Error: err.message })
+  }
 }
 
 //
 const postMessage = async function (req, res) {
-  let message = req.body.message
-  let user = await userModel.findById(req.params.userId)
-  if (!user) return res.send({ status: false, msg: 'No such user exists' })
+  try {
+    let user = await userModel.findById(req.params.userId)
+    if (!user) return res.status(404).send({ msg: 'No such user exists' })
 
-  let post = user.posts
-  post.push(message)
-  let updatedUser = await userModel.findOneAndUpdate({ _id: user._id }, { posts: post }, { new: true })
-  return res.send({ status: true, data: updatedUser })
+    let message = req.body.message
+    let post = user.posts
+    post.push(message)
+    let updatedUser = await userModel.findOneAndUpdate({ _id: user._id }, { posts: post }, { new: true })
+    return res.status(200).send({ data: updatedUser })
+  } catch (err) {
+    res.status(500).send({ Error: err.message })
+  }
+
 }
 
 module.exports = {
--- src/middleWare/auth.js
+++ src/middleWare/auth.js
@@ -2,21 +2,30 @@ const jwt = require("jsonwebtoken");
 const userModel = require("../models/userModel")
 
 const authenticate = function (req, res, next) {
-    if (!req.headers["x-auth-token"])
-        return res.send({ status: false, msg: "token must be present" });
+    try {
+        if (!req.headers["x-auth-token"])
+            return res.status(401).send({ status: false, msg: "token must be present" });
 
-    next()
+        next()
+    } catch(err) {
+        res.status(500).send({Error: err.message})
+    }
 }
- 
-const authorise = function (req, res, next) {
-    let token = req.headers["x-auth-token"];
-    let decodedToken = jwt.verify(token, "functionup-uranium")
-    let toBeUpdatedUserId = req.params.userId;
-    let loggedInUserId = decodedToken.userId;
-    if (loggedInUserId != toBeUpdatedUserId)
-        return res.send({ status: false, msg: "You're not authorised to do this task" });
 
-    next()
+const authorise = function (req, res, next) {
+    try {
+        let token = req.headers["x-auth-token"];
+        let decodedToken = jwt.verify(token, "functionup-uranium")
+        let toBeUpdatedUserId = req.params.userId;
+        let loggedInUserId = decodedToken.userId;
+        if (loggedInUserId != toBeUpdatedUserId)
+            return res.status(401).send({ status: false, msg: "You're not authorised to do this task" });
+
+        next()
+    }catch(err){
+        res.status(500).send({Error:err.message})
+    }
+    
 }
 
 module.exports = {
